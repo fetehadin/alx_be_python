0e16bbbbc627c861a7e3194004398dc6bd87acb9
fetehadin <fetehadinnegash@gmail.com>
2024-09-22 22:52:00 +0300

    Initial commit

--- daily_reminder.py
+++ daily_reminder.py
@@ -15,8 +15,3 @@ match priority:
             print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time."
             )
     case low:
-        if time_bound == "yes":
-            print(f"Reminder: {task} is a {priority} priority task that requirs immediate attention today!")
-        else:           
-            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time."
-            )
