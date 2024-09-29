c122858900eb3e78be8efa16d4040c542596f7f9
fetehadin <fetehadinnegash@gmail.com>
2024-09-29 20:32:37 +0300

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter the item: ")
+            shopping_list.append(item)# Prompt for and add an item
+            pass
+        elif choice == '2':
+            item_index = int(input("Enter an item index: "))
+            if 0 <= item_index <= len(shopping_list):                
+                del shopping_list[item_index]
+            else:
+                print("Invalid index. Please try again.")
+            # Prompt for and remove an item
+            pass
+        elif choice == '3':
+            print(shopping_list)# Display the shopping list
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
