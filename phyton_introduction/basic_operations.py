49ccb09607511d0704713c12103a0d7eba56a9ca
fetehadin <fetehadinnegash@gmail.com>
2024-09-29 09:35:34 +0300

    Add files via upload

---
+++ phyton_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add_result = number1 + number2
+sub_result = number1 - number2
+pro_result = number1 * number2
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(add_result))
+print("subtraction of " + str(number1) + " and " + str(number2) + " is " + str(sub_result))
+print("multiplication of " + str(number1) + " and " + str(number2) + " is " + str(pro_result))
