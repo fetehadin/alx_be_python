c4530f0324d0d31019c9ea314f6872f3e10dc8f1
fetehadin <fetehadinnegash@gmail.com>
2024-10-11 21:05:44 +0300

    Add files via upload

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self,title,author,year):
+        self.title = title
+        self.author = author
+        self.year = year
+    def __del__(self):
+        print(f"Deleting {self.title}")
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+my_book = Book("1984", "George Orwell", 1949)
+print(my_book)
+print(repr(my_book))
+#title
+#author
+#publication year
+    
